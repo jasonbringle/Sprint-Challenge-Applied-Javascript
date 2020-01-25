9c85d16f906b5b6b80db9bb95e7bd3d842ec4eb4
Jason <Jason@MacBook-Pro-3.local>
2020-01-25 09:37:03 -0800

    Built Elements for header

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,12 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+    //Elements
+    const headerDiv = document.createElement('div');
+    const date = document.createElement('span');
+    const title = document.createElement('h1');
+    const temp = document.createElement('span');
+
+
+}
